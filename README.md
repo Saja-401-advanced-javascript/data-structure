27efa8802b468fc6c7658d2dd1fd81e4e1b7b681
SajaSwalgah <saja.swalgah@yahoo.com>
2020-02-08 01:02:34 +0200

    the challenge done

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Code Challenge 11
 
-## PseudoQueue
+# Code Challenge: Class 12
+
+## Animal shilter
 
 ## Challenge Summary
-Create a brand new PseudoQueue class. methods : enqueue , dequeue
+To creat a class and methods 
 
 ## Challenge Description
-Create a brand new PseudoQueue class.
-
- PseudoQueue class will implement our standard queue interface (the two methods listed below), but will internally only utilize 2 Stack objects. Ensure that you create your class with the following methods: enqueue which inserts value into the PseudoQueue, using a first-in, first-out approach. dequeue which extracts a value from the PseudoQueue, using a first-in, first-out approach.
-
-## Whiteboard
-![](img/challenge11.jpeg)
+Create a class called animalShilte that enqueue and dequeue cats and dogs
---
+++ __tests__/fifo-animal-shelter.test.js
@@ -0,0 +1,79 @@
+
+
+let Animal = require('../queue-with-stacks/fifo-animal-shelter.js');
+
+
+let catA = {
+  name: 'lady',
+  kind: 'cat',
+};
+
+let catB = {
+  name: 'soso',
+  kind: 'cat',
+};
+
+let dogA = {
+  name: 'ghost',
+  kind: 'dog',
+};
+
+let dogB = {
+  name: 'Alex',
+  kind: 'dog',
+};
+
+
+
+describe('Animal shelter', () => {
+
+
+
+  it('should add a cat to the queue', () => {
+    let  animal = new Animal();
+
+    animal.enqueue(catA);
+
+    expect(animal.cat[0].kind).toEqual('cat');
+  });
+
+  it('should add a dog to the queue', () => {
+    let  animal = new Animal();
+
+    animal.enqueue(dogA);
+
+    expect(animal.dog[0].kind).toEqual('dog');
+  });
+
+  it('should remove a dog from the queue', () => {
+    let animal = new Animal();
+
+    animal.enqueue(dogA);
+    animal.enqueue(dogB);
+    animal.dequeue({ kind: 'dog' });
+
+    expect(animal.dog[0].kind).toEqual('dog');
+  });
+
+  it('should remove a cat from the queue', () => {
+    let animal = new Animal();
+
+    animal.enqueue(catA);
+    animal.enqueue(catB);
+    animal.dequeue({ kind: 'cat' });
+
+    expect(animal.cat[0].kind).toEqual('cat');
+  });
+
+  it('should dequeue nothing if the pref is not dog or cat', () => {
+    let  animal = new Animal();
+
+    animal.enqueue(catA);
+    animal.enqueue(catB);
+    animal.dequeue({ kind: 'potato' });
+
+    expect(animal.cat[0].kind).toEqual('cat');
+    expect(animal.cat[0].name).toEqual('lady');
+  });
+
+});
