c4ffbe7e41ce3bb50fed6271a76f347edfea92a4
SajaSwalgah <saja.swalgah@yahoo.com>
2020-02-09 17:08:42 +0200

    the challenge is done

--- __tests__/tree.test.js
+++ __tests__/tree.test.js
@@ -1,29 +1,101 @@
 'use strict';
 
-let {Node, BinaryTree, BinarySearchTree} = require('../lib/tree.js')
+let {Node, BinaryTree} = require('../lib/tree.js')
 
 
 describe('the Binary tree', () => {
     let tree = null;
 
     beforeAll(() => {
-        let treeA = new Node('saja');
-        let treeB = new Node('tareq');
-        let treeC = new Node('maryam');
-        let treeD = new Node('tameem');
-        let treeE = new Node('marya');
+        let nodeA = new Node('saja');
+        let nodeB = new Node('tareq');
+        let nodeC = new Node('maryam');
+        let nodeD = new Node('tameem');
+        let nodeE = new Node('marya');
 
-        treeA.leftChild = treeB;
-        treeA.rightChild = treeC;
-        treeC.leftChild = treeD;
-        treeC.rightChild = treeE;
+        nodeA.leftChild = nodeB;
+        nodeA.rightChild = nodeC;
+        nodeC.leftChild = nodeD;
+        nodeC.rightChild = nodeE;
 
-       let tree = new BinaryTree(treeA);
+       let tree = new BinaryTree(nodeA);
+    //    tree.preOrder
+    
     });
 
-    it ('preOrder()', () => {
-        let expected = [];
-        let preOrderTree = tree.preOrder()
-        expected(preOrderTree).toEqual(expected);
+    it ('Can successfully instantiate an empty tree', () => {
+        let tree = new BinaryTree;
+        expect(tree.root).toBeNull();
+    })
+
+    it ('Can successfully instantiate a tree with a single root node', () => {
+        let tree = new BinaryTree('saja');
+        expect(tree.root).toEqual('saja');
+    })
+
+    it ('Can successfully add a left child and right child to a single root node', () => {
+        let nodeA = new Node('saja');
+        let nodeB = new Node('tareq');
+        let nodeC = new Node('maryam');
+        let tree = new BinaryTree(nodeA);
+       
+        tree.leftChild = nodeB;
+        tree.rightChild = nodeC;
+
+        expect(tree.leftChild.value).toEqual('tareq')
+        expect(tree.rightChild.value).toEqual('maryam')
+       
+    })
+
+    
+    it ('Can successfully return a collection from a preorder traversal', () => {
+        let nodeA = new Node('saja');
+        let nodeB = new Node('tareq');
+        let nodeC = new Node('maryam');
+        let nodeD = new Node('tameem');
+        let nodeE = new Node('marya');
+
+        nodeA.leftChild = nodeB;
+        nodeA.rightChild = nodeC;
+        nodeC.leftChild = nodeD;
+        nodeC.rightChild = nodeE;
+        
+        let tree = new BinaryTree(nodeA);
+        expect(tree.preOrder()).toEqual(['saja','tareq', 'maryam','tameem','marya'])
+       
+    })
+
+    it ('Can successfully return a collection from an inorder traversal', () => {
+        let nodeA = new Node('saja');
+        let nodeB = new Node('tareq');
+        let nodeC = new Node('maryam');
+        let nodeD = new Node('tameem');
+        let nodeE = new Node('marya');
+
+        nodeA.leftChild = nodeB;
+        nodeA.rightChild = nodeC;
+        nodeC.leftChild = nodeD;
+        nodeC.rightChild = nodeE;
+        
+        let tree = new BinaryTree(nodeA);
+        expect(tree.inOrder()).toEqual(['tareq', 'saja','tameem','maryam','marya'])
+       
+    })
+
+    it ('Can successfully return a collection from a postorder traversal', () => {
+        let nodeA = new Node('saja');
+        let nodeB = new Node('tareq');
+        let nodeC = new Node('maryam');
+        let nodeD = new Node('tameem');
+        let nodeE = new Node('marya');
+
+        nodeA.leftChild = nodeB;
+        nodeA.rightChild = nodeC;
+        nodeC.leftChild = nodeD;
+        nodeC.rightChild = nodeE;
+        
+        let tree = new BinaryTree(nodeA);
+        expect(tree.postOrder()).toEqual(['tareq','tameem','marya','maryam', 'saja'])
+       
     })
 })
