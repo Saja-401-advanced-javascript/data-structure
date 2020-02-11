1c210a51bf9cd33ecaf89b70636514eec5b1c0f5
SajaSwalgah <saja.swalgah@yahoo.com>
2020-02-11 07:35:35 +0200

    not yet

--- __tests__/tree.test.js
+++ __tests__/tree.test.js
@@ -1,24 +1,38 @@
 'use strict';
 
-let {BinarySearchTree} = require('../lib/tree.js')
+let {Node, BinaryTree, BinarySearchTree} = require('../lib/tree.js')
 
-
-describe('the Binary tree', () => {
+describe('Breadth first', ()=>{
+    let tree = null;
   
-
-    it ('fizzbuzz', () => {
-        let tree = new BinarySearchTree(null);
-        tree.add(10);
-        tree.add(3);
-        tree.add(0);
-        tree.add(7);
-        tree.add(15);
-        tree.add(12);
-        tree.fizzBuzzTree(tree.root);
-        console.log(tree.root.left);
-        expect(tree.root.value).toBe('Buzz');
-        expect(tree.root.left.value).toBe('Fizz');
-        expect(tree.root.left.left.value).toBe('FizzBuzz');
-        expect(tree.root.right.value).toBe('FizzBuzz');
-        })
-})
+      let node1 = new BreadthfirstTraversal.Node(2);
+      let node2 = new BreadthfirstTraversal.Node(7);
+      let node3 = new BreadthfirstTraversal.Node(5);
+      let node4 = new BreadthfirstTraversal.Node(2);
+      let node5 = new BreadthfirstTraversal.Node(6);
+      let node6 = new BreadthfirstTraversal.Node(9);
+      let node7 = new BreadthfirstTraversal.Node(5);
+      let node8 = new BreadthfirstTraversal.Node(11);
+      let node9 = new BreadthfirstTraversal.Node(4);
+  
+  
+      node1.left = node2;
+      node1.right = node3;
+      node2.left = node4;
+      node2.right = node5;
+      node5.left = node7;
+      node5.right = node8;
+      node3.right = node6;
+      node6.left = node9;
+  
+  
+      tree = new BinaryTree(node1);
+      console.log('8888', tree);
+      
+  
+ 
+    it('it should traversal as breadth in Binary search tree', ()=>{
+      expect(tree.breadthTraversal()).toEqual([2,7,5,2,6,9,5,11,4]);
+    });
+  
+  });
