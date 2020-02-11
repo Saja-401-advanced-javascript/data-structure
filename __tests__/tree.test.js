33c4a01fd137c891a62141b662a0bc861dfcbf95
SajaSwalgah <saja.swalgah@yahoo.com>
2020-02-11 09:52:51 +0200

    eslint

--- __tests__/tree.test.js
+++ __tests__/tree.test.js
@@ -1,24 +1,24 @@
-'use strict';
 
-let {BinarySearchTree} = require('../lib/tree.js')
+
+let {BinarySearchTree} = require('../lib/tree.js');
 
 
 describe('the Binary tree', () => {
-  
 
-    it ('fizzbuzz', () => {
-        let tree = new BinarySearchTree(null);
-        tree.add(10);
-        tree.add(3);
-        tree.add(0);
-        tree.add(7);
-        tree.add(15);
-        tree.add(12);
-        tree.fizzBuzzTree(tree.root);
-        console.log(tree.root.left);
-        expect(tree.root.value).toBe('Buzz');
-        expect(tree.root.left.value).toBe('Fizz');
-        expect(tree.root.left.left.value).toBe('FizzBuzz');
-        expect(tree.root.right.value).toBe('FizzBuzz');
-        })
-})
+
+  it ('fizzbuzz', () => {
+    let tree = new BinarySearchTree(null);
+    tree.add(10);
+    tree.add(3);
+    tree.add(0);
+    tree.add(7);
+    tree.add(15);
+    tree.add(12);
+    tree.fizzBuzzTree(tree.root);
+    console.log(tree.root.left);
+    expect(tree.root.value).toBe('Buzz');
+    expect(tree.root.left.value).toBe('Fizz');
+    expect(tree.root.left.left.value).toBe('FizzBuzz');
+    expect(tree.root.right.value).toBe('FizzBuzz');
+  });
+});
